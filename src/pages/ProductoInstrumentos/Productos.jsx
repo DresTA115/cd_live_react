e0c2641f2fa7dbbe649918a154c6fc7856d21484
DresTA115 <juanandrestoro08@gmail.com>
2025-11-07 19:44:20 -0500

    Se añaden tarjetas de instrumentos similares en la vista individual de instrumentos.

--- src/pages/ProductoInstrumentos/Productos.jsx
+++ src/pages/ProductoInstrumentos/Productos.jsx
@@ -1,9 +1,23 @@
 import './Productos.css'
 import { useLocation, Link } from 'react-router-dom'
 import { PresentacionProducto } from '../../components/PresentacionProducto/PresentacionProducto'
+import instrumentos from '../../api/instrumentos.json'
+import { BottonComprar } from '@components/common/BottonComprar/BottonComprar'
+
+
+
 export function Productos() {
   const location = useLocation()
   const producto = location.state
+  
+  // Función para formatear precios
+  const formatearPrecio = (valor) => {
+    return new Intl.NumberFormat('es-CO', {
+      style: 'currency',
+      currency: 'COP',
+      maximumFractionDigits: 0,
+    }).format(valor)
+  }
 
   if (!producto) {
     return (
@@ -14,12 +28,43 @@ export function Productos() {
     )
   }
 
+  // 🔹 Filtramos productos similares (misma categoría, diferente nombre)
+  const productosSimilares = instrumentos
+    .filter(
+      (item) =>
+        item.categoria === producto.categoria && item.nombre !== producto.nombre
+    )
+    .slice(0, 4) // mostramos solo 4
+
   return (
     <div className="paginaProducto">
-          <h1>Detalle del Producto</h1>  
-        
+      <h1>Detalle del Producto</h1>
+
       <PresentacionProducto producto={producto} />
-      <Link to="/albums" className="volverLink">Volver</Link>
+
+      <h2 className="similares">Productos similares</h2>
+
+      {/* 🔹 Contenedor de tarjetas similares */}
+
+      <div className="contenedorSimilares">
+        {productosSimilares.map((item, index) => (
+          <div className="tarjetaSimilares" key={index}>
+            <img
+              src={item.imagen}
+              alt={item.nombre}
+              className="imagenSimilares"
+            />
+            <h3 className='nombreSimilares'>{item.nombre}</h3>
+            <p className='descripcionSimilares'>{item.descripcion}</p>
+            <p className="precio">{formatearPrecio(item.precio)}</p>
+            <BottonComprar onClick={(e) => manejarAgregarAlCarrito(e, instrumento)} />
+          </div>
+        ))}
+      </div>
+
+      <Link to="/albums" className="volverLink">
+        Volver
+      </Link>
     </div>
   )
 }
